27e6d9cdefdffb292b1d40b1c03e73fdd1baf562
princeAlan25 <prince.alain.loui@gmail.com>
2026-02-16 11:17:03 +0200

    Added Audio page.

--- agentic-gallery/src/pages/AudioPage.tsx
+++ agentic-gallery/src/pages/AudioPage.tsx
@@ -1,7 +1,19 @@
+import { PromptInterface } from "../components/AI/PromptInterface"
+import { AITaskResultSpace } from "../components/Audio/AITaskResultSpace"
 import { Mp3Skin } from "../components/Audio/Mp3Skin"
+import { AudioSyncProvider } from "../Shared/AudioSyncContextProvider"
 
 export const AudioPage = () => {
-    return(
-        <Mp3Skin />
+    return (
+        <div className="w-full h-full flex justify-between">
+            <AudioSyncProvider>
+                <Mp3Skin />
+                <div className="w-[58%] bg-white/10 rounded-4xl p-2 flex flex-col justify-between">
+                    <PromptInterface />
+                    <AITaskResultSpace />
+                </div>
+            </AudioSyncProvider>
+
+        </div>
     )
 }
