80978921976fe5dcf72407f6d55bd0e579030e53
princeAlan25 <prince.alain.loui@gmail.com>
2026-02-16 11:14:34 +0200

    Added Sided Navigation Manager bar.

--- agentic-gallery/src/components/SideBar.tsx
+++ agentic-gallery/src/components/SideBar.tsx
@@ -2,8 +2,8 @@ import { Link } from "react-router-dom"
 
 export const SideBar = () => {
     return (
-        <div className="p-5 w-[20%] backdrop-blur-3xl">
-            <nav className="w-50 text-white grid grid-cols-1 gap-5 text-white [&>*]:p-2 [&>*]:hover:border-b [&>*]:hover:pl-3 [&>*]:active:bg-white/50 [&>*]:transition duration-700 ease-in-out">
+        <div className="p-5 w-[16%] backdrop-blur-3xl">
+            <nav className="w-50 text-white grid grid-cols-1 gap-5 *:border *:border-white/50 *:rounded-md *:p-2 *:hover:pl-3 *:hover:bg-white/20 *:active:bg-white/0 *:transition duration-700 ease-in-out">
                 <Link to={"/"} className="p-2">Audio</Link>
                 <Link to={"/video"}>Video</Link>
                 <Link to={"/image"}>Images</Link>
