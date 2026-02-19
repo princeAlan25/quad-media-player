17b3827d5e1a6179612a8cf78b3a82b8faa47135
princeAlan25 <prince.alain.loui@gmail.com>
2026-02-19 09:02:59 +0200

    Adding Prompt UI enhancement

--- agentic-gallery/src/components/AI/PromptInterface.tsx
+++ agentic-gallery/src/components/AI/PromptInterface.tsx
@@ -1,11 +1,12 @@
+import { FaPaperPlane } from "react-icons/fa6";
 import { AudioBroker } from "../../Brokers/AudioBroker";
 
 export const PromptInterface = () => {
     const audioBroker = new AudioBroker();
     return (
         <div className="w-full h-fit max-h-fit bg-white/50 rounded-4xl grid-cols-2 gap-5 p-5">
-            <textarea className="w-[86%] h-full text-black outline-none bg-black/5 p-2 rounded-lg overflow-hidden wrap-break-word" id="promptInputElement" placeholder="Let me user your mp3 Player!"></textarea>
-            <button className="p-5 min-h-18 max-h-18 float-right rounded-full shadow-md text-green-700 font-bold bg-white hover:opacity-50 cursor-pointer active:opacity-100" onClick={() => audioBroker.getAgentAudio("audioElement", "promptInputElement")}>Send</button>
+            <textarea className="w-[78%] h-full text-black outline-none bg-black/5 p-2 rounded-lg overflow-hidden wrap-break-word" id="promptInputElement" placeholder="Let me use your mp3 Player to listen together!" required></textarea>
+            <button className="p-5 min-w-16 max-w-16 min-h-16 max-h-16 mr-2 float-right rounded-full shadow-md text-green-700 font-bold bg-white hover:opacity-50 cursor-pointer active:opacity-100" onClick={async () => await audioBroker.getAgentAudio("audioElement", "promptInputElement")}><FaPaperPlane className="w-full h-full" /></button>
         </div>
     )
 }
