3a8fbea55064ee5b1f40aaab87707840bf39378b
princeAlan25 <prince.alain.loui@gmail.com>
2026-04-08 14:11:50 +0200

    Changed text adn background for ci/cd testing

--- agentic-gallery/src/pages/audio/ui/AudioPage.tsx
+++ agentic-gallery/src/pages/audio/ui/AudioPage.tsx
@@ -2,7 +2,7 @@ import { Mp3Player } from '@/widgets/audio-player'
 
 export const AudioPage = () => {
   return (
-    <div className="w-full h-full flex justify-between overflow-x-auto">
+    <div className="w-full h-full flex justify-between overflow-x-auto bg-white text-red">
       <Mp3Player />
     </div>
   )
